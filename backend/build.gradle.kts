2951cf650c7e29834cf27a962af796d85c6412b4
poeynus <poeynus@gmail.com>
2021-11-22 14:35:05 +0900

    테스트 중

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
